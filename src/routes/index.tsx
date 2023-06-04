a6e0225289ba60f9ad042659558f13b01bcd7a31
Vincent <thisoneis4business@gmail.com>
2023-06-04 21:48:10 +1200

    feat: Homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,56 @@
 import { component$ } from "@builder.io/qwik";
-import { DocumentHead, routeLoader$ } from "@builder.io/qwik-city";
-import { __api__ } from "~/common/constant";
-import { Stories } from "~/types/story";
-
-export const useNews = routeLoader$(async (ev) => {
-  const res = await fetch(`${__api__}/news`);
-  const body = await res.json();
-
-  const maybeNews = await Stories.safeParseAsync(body);
-  if (!maybeNews.success) {
-    throw maybeNews.error;
-  }
-
-  return {
-    news: maybeNews.data,
-  };
-});
+import { DocumentHead } from "@builder.io/qwik-city";
+import { tw } from "~/tailwind";
 
 export default component$(() => {
-  const data = useNews();
   return (
-    <div class="my-3 grid w-[95%] grid-cols-3 place-items-center gap-1 bg-white">
-      {data.value.news.map(
-        ({ id, title, user, type, comment_count, time_ago, points }) => {
-          return <div>{time_ago}</div>;
-        }
-      )}
+    <div class="flex h-screen w-screen flex-col items-center justify-center">
+      <div class="mt-5 flex flex-col items-center justify-center md:mt-8">
+        <div class="flex items-center justify-center gap-3 md:gap-4">
+          <span class="animate-slide-down text-5xl font-bold md:text-8xl">
+            Tech
+          </span>
+          <div
+            class={tw(`relative animate-slide-up text-5xl 
+            font-bold [animation-delay:0.5s] before:absolute
+            before:left-0 before:top-0 before:h-full before:w-full 
+            before:animate-expand-right before:border-b before:border-black 
+            before:content-[''] before:[animation-delay:1.25s]
+            md:text-8xl md:before:border-b-2`)}
+          >
+            stories
+          </div>
+        </div>
+        <div
+          class={tw(
+            `animate-slide-right bg-gradient-to-r from-orange-400 to-lime-300
+            bg-clip-text text-6xl font-semibold text-transparent
+            [animation-delay:1s] md:text-9xl`
+          )}
+        >
+          Streamlined
+        </div>
+      </div>
+      <button
+        class={tw(`relative mt-10 animate-slide-down rounded text-sm font-medium
+        text-blue-950 [animation-delay:1.5s] before:absolute before:left-0 
+        before:top-0 before:h-full before:w-full before:border-b 
+        before:border-sky-400 before:transition-all before:content-[''] 
+        hover:before:scale-x-100 active:before:scale-x-100 md:mt-16 md:text-base 
+        md:before:scale-x-0 md:before:border-b-2`)}
+      >
+        Coming soon
+      </button>
     </div>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Frontpage",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Tech Stories, Streamlined",
     },
   ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,55 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      animation: {
+        "slide-up": "slide-up 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+        "slide-down": "slide-down 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+        "slide-right": "slide-left 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+        "expand-right": "expand-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
+      },
+      keyframes: {
+        "slide-up": {
+          "0%": {
+            transform: "translateY(100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
+        },
+        "slide-left": {
+          "0%": {
+            transform: "translateX(-50%)",
+            opacity: 0,
+          },
+          "80%": {
+            transform: "translateX(1%)",
+          },
+          "100%": {
+            transform: "translateX(0)",
+            opacity: 1,
+          },
+        },
+        "slide-down": {
+          "0%": {
+            transform: "translateY(-100%)",
+            opacity: 0,
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: 1,
+          },
+        },
+        "expand-right": {
+          "0%": {
+            width: 0
+          },
+          "100%": {
+            width: "100%",
+          },
+        },
+      }
     },
   },
   plugins: [],
