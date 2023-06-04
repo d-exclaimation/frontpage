a43fb7d14a8215dfdd62cddd7637d12c3d871c92
Vincent <thisoneis4business@gmail.com>
2023-06-04 17:02:06 +1200

    Reset

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,10 +1,8 @@
 import { component$, Slot } from "@builder.io/qwik";
-import Navigation from "~/components/navigation";
 
 export default component$(() => {
   return (
     <>
-      <Navigation />
       <Slot />
     </>
   );
